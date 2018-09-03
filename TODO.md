d46fa4764593fe7e183d7215219fcc2097fd059e
Valentin Matton <taq@protonmail.ch>
2018-09-03 18:02:29 +0200

    Add basic MUI support

--- TODO.md
+++ TODO.md
@@ -1,4 +1,11 @@
+#### Features
+
+#### Development
 * add Flow
 * add ESLint/Prettier
 * add backend
 * use styled-components to create our own visual identity
+
+#### Next
+* create custom <App> for layout
+* create custom <Document>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,27 @@
-const Landing = () => (<div>This is a bookshelf</div>);
+import Head from 'next/head';
+
+import Typography from '@material-ui/core/Typography';
+
+function Landing() {
+  return (
+
+    <React.Fragment>
+
+      <Head>
+        <title>Yet Another Bookshelf</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
+      </Head>
+
+      <Typography>
+        This is a bookshelf.
+      </Typography>
+
+    </React.Fragment>
+  );
+}
 
 export default Landing;
