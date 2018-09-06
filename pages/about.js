db927e122821f66fd10cd290f1053f0047ed3fcf
Valentin Matton <taq@protonmail.ch>
2018-09-06 13:47:16 +0200

    Fix grid layout and make it somewhat responsive

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,10 @@
 import Typography from '@material-ui/core/Typography';
 
-import Head from '../components/Head';
 import AppBar from '../components/AppBar';
 
 function About() {
   return (
     <React.Fragment>
-      <Head/>
       <AppBar/>
       <Typography variant="headline">About YABookshelf !</Typography>
     </React.Fragment>
