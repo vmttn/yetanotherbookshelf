555a858bab424db92f0635bf43fc59c46650834b
Valentin Matton <taq@protonmail.ch>
2018-09-07 12:59:28 +0200

    Add Redux for search

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -3,13 +3,17 @@ import React from 'react';
 import {withStyles} from '@material-ui/core/styles'
 import Input from '@material-ui/core/Input';
 
+import {setSearchTerm} from '../lib/actions';
+import {connect} from 'react-redux';
+
 const styles = {
   textField: {
     color: "inherit"
   }
 };
 
-function SearchInput({classes, searchTerm}) {
+function SearchInput(props) {
+  let {classes, handleInputChange, searchTerm} = props;
   return (
     <Input
       id="search"
@@ -18,8 +22,17 @@ function SearchInput({classes, searchTerm}) {
       className={classes.textField}
       disableUnderline={true}
       value={searchTerm}
+      onChange={e => handleInputChange(e.target.value)}
     />
   )
 }
 
-export default withStyles(styles)(SearchInput);
+const mapStateToProps = state => ({searchTerm: state.searchTerm});
+
+const mapDispatchToProps = dispatch => ({
+  handleInputChange: value => {
+    dispatch(setSearchTerm(value));
+  }
+});
+
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(SearchInput));
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import {Provider} from 'react-redux';
+import store from '../lib/store';
+
 import AppBar from '../components/AppBar';
 import BookCard from '../components/BookCard';
 import SearchInput from '../components/SearchInput';
@@ -17,20 +20,24 @@ const styles = {
 function Index({classes}) {
   return (
     <React.Fragment>
-      <AppBar>
-        <SearchInput/>
-      </AppBar>
+      <Provider store={store}>
+        <React.Fragment>
+          <AppBar>
+            <SearchInput/>
+          </AppBar>
 
-      <Grid className={classes.grid} container justify="space-around" alignContent="space-between">
-        {
-          [1, 2, 3, 4, 5, 6, 7, 9, 10, 11].map(
-            value =>
-              <Grid className={classes.item} item key={value} xs={12} md={6} xl={4}>
-                <BookCard/>
-              </Grid>
-          )
-        }
-      </Grid>
+          <Grid className={classes.grid} container justify="space-around" alignContent="space-between">
+            {
+              [1, 2, 3, 4, 5, 6, 7, 9, 10, 11].map(
+                value =>
+                  <Grid className={classes.item} item key={value} xs={12} md={6} xl={4}>
+                    <BookCard/>
+                  </Grid>
+              )
+            }
+          </Grid>
+        </React.Fragment>
+      </Provider>
     </React.Fragment>
   );
 }
