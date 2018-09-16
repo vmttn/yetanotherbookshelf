eefb812508a8e8478c42e7f07297e838d2edf21c
Valentin Matton <taq@protonmail.ch>
2018-09-16 17:26:37 +0200

    Refactor _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,38 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import flush from 'styled-jsx/server';
 
 class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    let pageContext;
+    const page = renderPage(Component => {
+      const WrappedComponent = props => {
+        pageContext = props.pageContext;
+        return <Component {...props} />;
+      };
+
+      // TODO: remove this when switching to Flow
+      WrappedComponent.propTypes = {
+        pageContext: PropTypes.object.isRequired
+      };
+
+      return WrappedComponent;
+    });
+
+    return {
+      ...page,
+      pageContext,
+      styles: (
+        <>
+          <style
+            id="jss-server-side"
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: pageContext.sheetsRegistry.toString() }}
+          />
+          {flush() || null}
+        </>
+      )
+    };
+  }
+
   render() {
     const { pageContext } = this.props;
 
@@ -13,9 +45,7 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <title>Yet Another Bookshelf</title>
           <meta name="theme-color" content={pageContext.theme.palette.primary.main} />
-          {/* PWA primary color */}
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
-          {/* Use minimum-scale=1 to enable GPU rasterization */}
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
           <style>
@@ -32,60 +62,5 @@ class MyDocument extends Document {
     );
   }
 }
-
-MyDocument.getInitialProps = ctx => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
-  // Render app and page and get the context of the page with collected side effects.
-  let pageContext;
-  const page = ctx.renderPage(Component => {
-    const WrappedComponent = props => {
-      pageContext = props.pageContext;
-      return <Component {...props} />;
-    };
-
-    WrappedComponent.propTypes = {
-      pageContext: PropTypes.object.isRequired
-    };
-
-    return WrappedComponent;
-  });
-
-  return {
-    ...page,
-    pageContext,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: (
-      <React.Fragment>
-        <style
-          id="jss-server-side"
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: pageContext.sheetsRegistry.toString() }}
-        />
-        {flush() || null}
-      </React.Fragment>
-    )
-  };
-};
 
 export default MyDocument;
