c1eef0a398ac53555a119bd0cd33032a5d68aa9d
Valentin Matton <taq@protonmail.ch>
2018-10-06 17:05:27 +0200

    Remove redundant books fetch

--- pages/index.js
+++ pages/index.js
@@ -2,45 +2,14 @@
 
 import React from 'react';
 
-import fetch from 'isomorphic-unfetch';
-
-import { connect } from 'react-redux';
-import { setBookList } from '../lib/store/actions';
-
 import AppBar from '../components/AppBar';
 import BookGrid from '../components/BookGrid';
 
-class Index extends React.Component<{ books: Array<bookType> }> {
-  componentDidMount() {
-    const { books, populateBookList } = this.props;
-    populateBookList(books);
-  }
+const Index = () => (
+  <>
+    <AppBar withSearch />
+    <BookGrid />
+  </>
+);
 
-  render() {
-    const { books } = this.props;
-    return (
-      <>
-        <AppBar withSearch />
-        <BookGrid books={books} />
-      </>
-    );
-  }
-}
-
-Index.getInitialProps = async ({ req }) => {
-  const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : '';
-  const response = await fetch(`${baseUrl}/api/v1/public/book`);
-  const data = await response.json();
-  return { books: data };
-};
-
-const mapDispatchToProps = dispatch => ({
-  populateBookList: bookList => {
-    dispatch(setBookList(bookList));
-  }
-});
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(Index);
+export default Index;
