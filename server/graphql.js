9018a2d0b7efc0e1ff0a665e25464d99a99d4f50
Valentin Matton <taq@protonmail.ch>
2018-10-07 18:37:18 +0200

    Add basic book mutations

--- server/graphql.js
+++ server/graphql.js
@@ -11,12 +11,25 @@ const typeDefs = gql`
     title: String
     author: String
   }
+  type Mutation {
+    addBook(isbn: String, title: String, author: String): Book
+    deleteBook(isbn: String): Boolean
+  }
 `;
 
 const resolvers = {
   Query: {
     books(parent, args, context, info) {
       return Book.find().exec();
+    }
+  },
+  Mutation: {
+    addBook(parent, args, context, info) {
+      const book = new Book({ isbn: args.isbn, title: args.title, author: args.author });
+      return book.save();
+    },
+    deleteBook(parent, args, context, info) {
+      return !!Book.findOneAndDelete({ isbn: args.isbn }).exec();
     }
   }
 };
